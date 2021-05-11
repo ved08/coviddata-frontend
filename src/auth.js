5d819543bea6326330026a9f51c7caf311eb3a65
Vedvardhan Gyanmote <vedvardhan.g@gmail.com>
2021-05-11 23:05:24 +0530

    configured .env file

--- src/auth.js
+++ src/auth.js
@@ -1,13 +1,17 @@
 import firebase from "firebase/app"
 import "firebase/auth"
+import * as dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.join(__dirname,'../.env') });
+
 firebase.initializeApp({
-    apiKey: process.env.API_KEY,
+    apiKey: process.env.REACT_APP_API_KEY,
     authDomain: "cfa-coviddata.firebaseapp.com",
     projectId: "cfa-coviddata",
     storageBucket: "cfa-coviddata.appspot.com",
     messagingSenderId: "541721075264",
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 })
 
 class Auth {
