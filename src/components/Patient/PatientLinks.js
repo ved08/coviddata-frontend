9ec8f58ecf0b087f8ea20eefe07f919b9db2ddee
Vedvardhan Gyanmote <vedvardhan.g@gmail.com>
2021-05-12 12:01:12 +0530

    Added image sharing feature

--- src/components/Patient/PatientLinks.js
+++ src/components/Patient/PatientLinks.js
@@ -5,6 +5,18 @@ const PatientLinks = (props) => {
     console.log(links, data)
     const { name, blood, age, hospitalName, spo2, requirements, relationship, state, city, phn } = data;
     const imageUrl = `https://rophyllo.sirv.com/Images/a4-paper.jpg?text=Urgent%20Help%20Needed%0A%0A%0A%0A%0AName:%20${name}%0Aage:%20${age}%0Arequirements:%20${requirements}%0Arelationship:%20${relationship}%0Ablood:%20${blood}%0AhospitalName:%20${hospitalName}%0Aspo2:%20${spo2}%0Astate:%20${state}%0Acity:%20${city}%0Aphn:%20${phn}&text.font.size=16px&text.color=black&text.align=left&text.position=center` 
+    
+    const convertToBase64 = (img) => {
+        img.crossOrigin = 'Anonymous';
+        let canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+        var dataURL = canvas.toDataURL("image/png")
+        // return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+        return dataURL
+    }
     return(
         <div className="Patient-Links">
             <h2>Links</h2>
@@ -26,6 +38,14 @@ const PatientLinks = (props) => {
             </ul>
             <button onClick={async () => {
                 if(navigator.share) {
+                    const base64url = convertToBase64(document.getElementById("share-image"));
+                    const blob = await (await fetch(base64url)).blob();
+                    const file = new File([blob], 'fileName.png', { type: blob.type });
+                    navigator.share({
+                    title: "Share on Social Media",
+                    text: 'Image caption here',
+                    files: [file],
+                    })
                     // share feature available
                     await navigator.share({
                         title: "Title",
@@ -36,7 +56,7 @@ const PatientLinks = (props) => {
                     console.log("Share option not available")
                 }
             }}>Share</button>
-            <img src={imageUrl} alt="Image unavailable"/>
+            <img id="share-image" src={imageUrl} alt="Image unavailable"/>
         </div>
     );
 }
