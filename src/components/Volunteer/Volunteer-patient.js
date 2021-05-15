2a0d63141c106eee4e5be2ace413d466002936a7
Vedvardhan Gyanmote <vedvardhan.g@gmail.com>
2021-05-15 12:15:32 +0530

    Added table

--- src/components/Volunteer/Volunteer-patient.js
+++ src/components/Volunteer/Volunteer-patient.js
@@ -5,11 +5,24 @@ import "./common.css"
 
 const VolunteerPatient = () => {
     const [ selectedState, setSelectedState ] = useState("");
+    const [ data, setData ] = useState([])
     const getPatientData = () => {
         axios.post("https://coviddata.vedvardhan.repl.co/datapatients", {
             state: selectedState
         }).then(res => {
-            console.log(res.data)
+            const { data } = res;
+            let str = data.split("")
+                for (let i = 0; i <= str.length; i++) {
+                    if(str[i] == "'") {
+                        if(str[i+1] !== 's') {
+                            str[i] = '"'
+                        }
+                    }
+                }
+                str = str.join("")
+                // console.log(str)
+                str = JSON.parse(str)
+                setData(str)
         })
     }
     return(
@@ -28,6 +41,32 @@ const VolunteerPatient = () => {
                 </select>
                 <button className="Btn" onClick={getPatientData}>Get Data</button>
             </div>
+                {data && <table>
+                    <thead>
+                        <th>Patient's Name</th>
+                        <th>Blood Group</th>
+                        <th>Age</th>
+                        <th>Hospital</th>
+                        <th>State</th>
+                        <th>Address</th>
+                        <th>Phone</th>
+                        <th>Requirements</th>
+                        <th>spO<sub>2</sub> Level</th>
+                    </thead>
+                    {data.map((e, i) => (
+                        <tbody key={i}>
+                            <td>{e["Patient's Name"]}</td>
+                            <td>{e["Blood Group"]}</td>
+                            <td>{e["Age"]}</td>
+                            <td>{e["Hospital"]}</td>
+                            <td>{e["State"]}</td>
+                            <td>{e["City"]}</td>
+                            <td>{e["Phone Number"]}</td>
+                            <td>{e["Requirements"]}</td>
+                            <td>{e["Spo2 level"]}</td>
+                        </tbody>
+                    ))}
+                </table>}
         </div>
     )
 }
